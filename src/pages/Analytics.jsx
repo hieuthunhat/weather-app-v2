1d1deb9d8ad2afa95c65d86efdd1c8eb635f869f
hieunt15 <hieunt1@avadagroup.com>
2026-02-28 18:23:35 +0700

    add analytics feature

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {useSelector} from "react-redux";
+import {SettingContext} from "../contexts/SettingContext.jsx";
+import {useFetch} from "../hooks/useFetch.js";
+import {buildForecastURL} from "../helpers/helpers.jsx";
+import {FORECAST_URL} from "../consts/settingConstants.js";
+import {Container, Stack} from "@mui/material";
+import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner.jsx";
+import HourlyWeatherCard from "../components/HourlyWeatherCard/HourlyWeatherCard.jsx";
+import EmptyState from "../components/EmptyState/EmptyState.jsx";
 
 function Analytics() {
-    const location = useSelector((state) => state.weather.location);
-    console.log(location);
+    const selectedLocation = useSelector(state => state.weather.location);
+    const {selectedFields, setSelectedFields} = useContext(SettingContext);
+    // setSelectedFields({...selectedFields, current: []})
+
+    const {data: weatherData, loading, fetchApi} = useFetch({
+        url: buildForecastURL({
+            url: FORECAST_URL,
+            obj: selectedFields,
+            latitude: selectedLocation?.latitude,
+            longitude: selectedLocation?.longitude
+        }),
+        initLoad: false
+    })
+
+    useEffect(() => {
+        if (!selectedLocation) {
+            return;
+        }
+        fetchApi();
+    }, [selectedLocation])
     return (
-        <div></div>
+        <Container>
+            <Stack justifyContent={'center'} gap={2}>
+                {selectedLocation ?
+                    loading ? <LoadingSpinner/> :
+                        <>
+                            {weatherData && <HourlyWeatherCard data={weatherData}/>}
+                        </>
+                    :
+                    <EmptyState/>
+                }
+            </Stack>
+        </Container>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
-
-import BodyLayout from "../layouts/BodyLayout.jsx";
+import React, {useContext, useEffect} from 'react';
+import EmptyState from "../components/EmptyState/EmptyState.jsx";
+import {useSelector} from "react-redux";
+import {Container, Stack} from "@mui/material";
+import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner.jsx";
+import CurrentWeatherCard from "../components/CurrentWeatherCard/CurrentWeatherCard.jsx";
+import HourlyWeatherCard from "../components/HourlyWeatherCard/HourlyWeatherCard.jsx";
+import {SettingContext} from "../contexts/SettingContext.jsx";
+import {useFetch} from "../hooks/useFetch.js";
+import {buildForecastURL} from "../helpers/helpers.jsx";
+import {FORECAST_URL} from "../consts/settingConstants.js";
 
 function Home() {
+    const selectedLocation = useSelector(state => state.weather.location);
+    const {selectedFields, setSelectedFields} = useContext(SettingContext);
+    // setSelectedFields({...selectedFields, current: []})
+
+    const {data: weatherData, loading, fetchApi} = useFetch({
+        url: buildForecastURL({
+            url: FORECAST_URL,
+            obj: selectedFields,
+            latitude: selectedLocation?.latitude,
+            longitude: selectedLocation?.longitude
+        }),
+        initLoad: false
+    })
+
+    useEffect(() => {
+        if (!selectedLocation) {
+            return;
+        }
+        fetchApi();
+    }, [selectedLocation])
     return (
-        <>
-            <BodyLayout/>
-        </>
+        <Container>
+            <Stack justifyContent={'center'} gap={2}>
+                {selectedLocation ?
+                    loading ? <LoadingSpinner/> :
+                        <>
+                            {weatherData && <CurrentWeatherCard data={weatherData}/>}
+                            {weatherData && <HourlyWeatherCard data={weatherData}/>}
+                        </>
+                    :
+                    <EmptyState/>
+                }
+            </Stack>
+        </Container>
     )
 }
 
