f92d34e9781c24b5ebcf6b6006c0bfa139c0d4c7
hieunth <hieunguyentrong520@gmail.com>
2026-03-08 19:47:37 +0700

    update layout

--- src/components/ForecastHourlyCard/ForecastHourlyCard.jsx
+++ src/components/ForecastHourlyCard/ForecastHourlyCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Card, CardContent, Typography } from "@mui/material";
-import { LineChart } from "@mui/x-charts/LineChart";
+import {Card, CardContent, Stack, Typography} from "@mui/material";
+import {LineChart} from "@mui/x-charts/LineChart";
 
-const ForecastHourlyCard = ({ data }) => {
+const ForecastHourlyCard = ({data}) => {
     const hourly = data?.hourly;
     const units = data?.hourly_units;
 
@@ -18,51 +18,52 @@ const ForecastHourlyCard = ({ data }) => {
     });
 
     return (
-        <Card variant="outlined" sx={{ borderRadius: 2 }}>
-            <CardContent sx={{ p: 2, pb: '8px !important' }}>
-                <Typography variant="subtitle2" fontWeight={600} mb={1}>
-                    5 giờ tiếp theo
-                </Typography>
+        <Card variant="outlined" sx={{borderRadius: 2}}>
+            <Stack padding={1}>
+                <CardContent sx={{p: 2, pb: '8px !important'}}>
+                    <Typography variant="subtitle2" fontWeight={600} mb={1}>
+                        Next 5 hours </Typography>
 
-                <LineChart
-                    height={250}
-                    margin={{ top: 10, bottom: 55, left: 45, right: 10 }}
-                    series={[
-                        {
-                            data: next5Hours.temperature,
-                            label: `Temperature (${units?.temperature_2m ?? ""})`,
-                            showMark: true,
-                            curve: "natural",
-                        },
-                        {
-                            data: next5Hours.apparentTemp,
-                            label: `Feel likes (${units?.apparent_temperature ?? ""})`,
-                            showMark: true,
-                            curve: "natural",
-                        },
-                    ]}
-                    xAxis={[{
-                        data: timeLabels,
-                        scaleType: 'point',
-                        tickLabelStyle: { fontSize: 11 },
-                    }]}
-                    yAxis={[{
-                        tickLabelStyle: { fontSize: 11 },
-                    }]}
-                    slotProps={{
-                        legend: {
-                            direction: 'row',
-                            position: { vertical: 'bottom', horizontal: 'middle' },
-                            padding: 0,
-                            itemMarkWidth: 10,
-                            itemMarkHeight: 10,
-                            markGap: 4,
-                            itemGap: 12,
-                            labelStyle: { fontSize: 11 },
-                        },
-                    }}
-                />
-            </CardContent>
+                    <LineChart
+                        height={250}
+                        margin={{top: 10, bottom: 55, left: 45, right: 10}}
+                        series={[
+                            {
+                                data: next5Hours.temperature,
+                                label: `Temperature (${units?.temperature_2m ?? ""})`,
+                                showMark: true,
+                                curve: "natural",
+                            },
+                            {
+                                data: next5Hours.apparentTemp,
+                                label: `Feel likes (${units?.apparent_temperature ?? ""})`,
+                                showMark: true,
+                                curve: "natural",
+                            },
+                        ]}
+                        xAxis={[{
+                            data: timeLabels,
+                            scaleType: 'point',
+                            tickLabelStyle: {fontSize: 11},
+                        }]}
+                        yAxis={[{
+                            tickLabelStyle: {fontSize: 11},
+                        }]}
+                        slotProps={{
+                            legend: {
+                                direction: 'row',
+                                position: {vertical: 'bottom', horizontal: 'middle'},
+                                padding: 0,
+                                itemMarkWidth: 10,
+                                itemMarkHeight: 10,
+                                markGap: 4,
+                                itemGap: 12,
+                                labelStyle: {fontSize: 11},
+                            },
+                        }}
+                    />
+                </CardContent>
+            </Stack>
         </Card>
     );
 };
